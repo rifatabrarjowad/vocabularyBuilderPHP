5ef0565bc11fb41ad3f79a01df7fc4aaf273c264
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-05 01:55:00 +0600

    s

--- config.php
+++ config.php
@@ -1,2 +1,7 @@
 <?php
-define();
+define("DB_HOST", "localhost"); //localhost
+define("DB_NAME", "words");
+
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+
--- tasks.php
+++ tasks.php
@@ -1,5 +1,20 @@
 <?php
+include_once "config.php";
+
 $action = $_POST['action'] ?? '';
-if ('register'==$action) {
-    # code...
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (! $connection) {
+    throw new Exception ("Can't connect to database");
+}else {
+    if ('register'==$action) {
+      $username = $_POST['email']??'';
+      $pass = $_POST['pass']??'';
+      if ($username && $pass ) {
+          $query = "INSERT INTO users(email, password) VALUES('{$username}','{$pass}')";
+          mysqli_query($connection,$query);
+          
+      }
+    }
 }
+
+
