9c7f55a629da5e4ee3ec4dca6b5f54edc728d97b
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-04 23:59:45 +0600

    s

--- login.php
+++ login.php
@@ -32,7 +32,7 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="POST" action="tasks.php">
 					<span class="login100-form-title p-b-33">
 						Account Login
 					</span>
@@ -47,6 +47,7 @@
 						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100-1"></span>
 						<span class="focus-input100-2"></span>
+						<input  type="hidden" name="action" value="register" placeholder="Password">
 					</div>
 
 					<div class="container-login100-form-btn m-t-20">
